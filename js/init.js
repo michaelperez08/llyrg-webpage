fceca3e2367657ac5e1c1b69b11bcdbb734545d8
Michael Perez <michael.perezmurillo@gmail.com>
2018-09-07 08:43:47 -0600

    first commit

---
+++ js/init.js
@@ -0,0 +1,12 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+    $('.slider').slider();
+    $('.indicators').hide();
+    $('select').material_select();
+    $('.scrollspy').scrollSpy();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
